f59c6996e8077177155917d62d29787fb879ad26
Aliaksandr Yankouski <aleksandr.yankovskiy@gmail.com>
2017-02-20 10:51:49 +0300

    Update test.js

--- test.js
+++ test.js
@@ -58,7 +58,7 @@ it('should check if brackets sequence is correct 12', () => {
 });
 
 it('should check if brackets sequence is correct 13', () => {
-  assert.equal(check('5555512575557777777555566667888888667661133833448441111222233333444442266666', config6), true);
+  assert.equal(check('5555512575557777777555566667888888667661133833448441111222233333444442266666', config6), false);
 });
 
 it('should check if brackets sequence is correct 14', () => {
